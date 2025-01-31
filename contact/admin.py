ac7244a878508e6cb1c9cdb02c90332b7cfde97b
Luiz Fernando <lf0821944@gmail.com>
2025-01-31 13:35:26 -0300

    contact

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from contact import models
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'first_name', 'last_name', 'phone',
 # Register your models here.
