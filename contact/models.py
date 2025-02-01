6455ce7f666340e80c856e1d537237fdfe2fac43
Luiz Fernando <lf0821944@gmail.com>
2025-02-01 10:04:30 -0300

    criando imageField

--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,13 @@
 from django.utils import timezone
 from django.db import models
 
+# id (primary key - automático)
+# first_name (string), last_name (string), phone (string)
+# email (email), created_date (date), description (text)
+# category (foreign key), show (boolean), picture (imagem)
+
+# Depois
+# owner (foreign key)
 # Create your models here.
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -9,6 +16,9 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture =models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
+    #ctrl + shift + c
