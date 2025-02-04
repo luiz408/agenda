1502a410ac39be3d011d2c73ac59b21900942347
Luiz Fernando <lf0821944@gmail.com>
2025-02-04 09:37:03 -0300

    manipulando

--- contact/admin.py
+++ contact/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from contact import models
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id' , 'first_name', 'last_name', 'phone'
+    list_display = 'id' , 'first_name', 'last_name', 'phone', 'show'
     ordering = '-id',
 #    list_filter = 'created_date',
     search_fields = 'id' , 'first_name', 'last_name',
     list_per_page = 15
     list_max_show_all = 300
     list_editable = 'first_name', 'last_name',
-    list_display_links = 'id' ,  'phone',
+    list_display_links = 'id' ,  'phone', 'show',
 # Register your models here.
 
 @admin.register(models.Category)
