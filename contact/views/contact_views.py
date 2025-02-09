1c2f3f0068a52cf65be1c4cd1abab391ef4b37a2
Luiz Fernando <lf0821944@gmail.com>
2025-02-09 18:45:20 -0300

    paginator

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,15 +1,19 @@
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
+from django.core.paginator import Paginator
+
 from contact.models import Contact
 
 
 def index(request):
     contacts = Contact.objects \
         .filter(show=True)\
-        .order_by('-id')[10:20]
-
+        .order_by('-id')
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
     }
 
     return render(
@@ -31,9 +35,14 @@ def search(request):
             Q(email__icontains=search_value)
         )\
         .order_by('-id')
+        
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     context = {
-        'contacts': contacts,
-        'site_title': 'Search - '
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
+        'search_value' : search_value,   
     }
     return render(
         request,
