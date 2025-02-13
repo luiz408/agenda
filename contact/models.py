d0864826a64b68a8ec97c4f53e974e3c419d582c
Luiz Fernando <lf0821944@gmail.com>
2025-02-13 11:43:46 -0300

    término

--- contact/models.py
+++ contact/models.py
@@ -1,23 +1,25 @@
 from django.contrib.auth.models import User
-from django.utils import timezone
 from django.db import models
+from django.utils import timezone
 
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
 # category (foreign key), show (boolean), picture (imagem)
-
-# Depois
 # owner (foreign key)
-# Create your models here.
+
+
 class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
-        
+
     name = models.CharField(max_length=50)
+
     def __str__(self) -> str:
         return self.name
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -26,7 +28,7 @@ class Contact(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
-    picture =models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
@@ -37,6 +39,6 @@ class Contact(models.Model):
         on_delete=models.SET_NULL,
         blank=True, null=True
     )
+
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
-    #ctrl + shift + c
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,55 +1,101 @@
-from typing import Any, Dict
-from django import forms
-from django.core.exceptions import ValidationError
-
-from django.core.paginator import Paginator
-from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+
+from contact.forms import ContactForm
 from contact.models import Contact
 
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = (
-            'first_name', 'last_name', 'phone',
-        )
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
-        return super().clean()
-    
-    
+
+@login_required(login_url='contact:login')
 def create(request):
+    form_action = reverse('contact:create')
+
     if request.method == 'POST':
+        form = ContactForm(request.POST, request.FILES)
+
         context = {
-            'form': ContactForm(request.POST)
+            'form': form,
+            'form_action': form_action,
         }
+
+        if form.is_valid():
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
+            return redirect('contact:update', contact_id=contact.pk)
+
         return render(
             request,
             'contact/create.html',
             context
-        )   
-    context = {
-        'form': ContactForm()     
-    }
-    
+        )
 
+    context = {
+        'form': ContactForm(),
+        'form_action': form_action,
+    }
 
     return render(
         request,
         'contact/create.html',
         context
+    )
+
+
+@login_required(login_url='contact:login')
+def update(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True, owner=request.user
+    )
+    form_action = reverse('contact:update', args=(contact_id,))
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST, request.FILES, instance=contact)
+
+        context = {
+            'form': form,
+            'form_action': form_action,
+        }
+
+        if form.is_valid():
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk)
+
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+
+    context = {
+        'form': ContactForm(instance=contact),
+        'form_action': form_action,
+    }
+
+    return render(
+        request,
+        'contact/create.html',
+        context
+    )
+
+
+@login_required(login_url='contact:login')
+def delete(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True, owner=request.user
+
+    )
+    confirmation = request.POST.get('confirmation', 'no')
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact': contact,
+            'confirmation': confirmation,
+        }
     )
